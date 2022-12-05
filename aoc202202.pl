d2df775f87f5fb5fb95a5635df32336aa0f2f3f6
Benjamin Titmus <ben.titmus@cantab.net>
2022-12-05 17:20:28 +0000

    Improve day 2 Prolog solution

--- aoc202202.pl
+++ aoc202202.pl
@@ -10,31 +10,31 @@ matches([Match|Matches]) --> match(Match), matches(Matches).
 
 match((Opponent, Strategy)) --> nonblank(Opponent), " ", nonblank(Strategy), eol.
 
-% Transform A, B, C and X, Y, Z into 1, 2, 3 for a single item
+% Transform A, B, C and X, Y, Z into 0, 1, 2 for a single item
 transform_score((OpponentChar, StrategyChar), (Opponent, Strategy)) :-
-   Opponent is OpponentChar - 64,
-   Strategy is StrategyChar - 87.
+   Opponent is OpponentChar - 65,
+   Strategy is StrategyChar - 88.
 
 % True if the Opponent and Strategy would produce the given score for the match
 match_score(Opponent, Opponent, 3) :- !.
-match_score(Opponent, Strategy, 6) :- Strategy is (Opponent rem 3) + 1, !.
-match_score(Opponent, Strategy, 0) :- Strategy is ((Opponent + 1) rem 3) + 1, !.
+match_score(Opponent, Strategy, 6) :- Strategy is (Opponent + 1) rem 3, !.
+match_score(Opponent, Strategy, 0) :- Strategy is (Opponent + 2) rem 3, !.
 
 % First case is where the Strategy represents what we should play
 score_first((Opponent, Strategy), Score) :-
    match_score(Opponent, Strategy, MatchScore),
-   Score is Strategy + MatchScore.
+   Score is Strategy + MatchScore + 1.
 
-strategy(1, 0).
-strategy(2, 3).
-strategy(3, 6).
+strategy(0, 0).
+strategy(1, 3).
+strategy(2, 6).
 
 % Second case is where the Strategy tells us how many points to get from the match
 % where strategy/2 is used to map to match points
 score_second((Opponent, Strategy), Score) :-
    strategy(Strategy, MatchScore),
    match_score(Opponent, Choice, MatchScore), 
-   Score is Choice + MatchScore.
+   Score is Choice + MatchScore + 1.
 
 % Determine the score if a given algorithm is used for each match
 total_score(AlgorithmForMatch, Score) :-
